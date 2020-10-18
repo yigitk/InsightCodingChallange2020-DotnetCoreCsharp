1a1352f95a91ad99631190f69f848e17b596b654
Yigit Kucuk <yigitk@users.noreply.github.com>
2020-10-18 11:19:28 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
 dotnet test ./src/Task.Tests
 dotnet ./src/Task.Main.dll
 
