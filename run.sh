91577e6516ce3bf4adf38f0bd3922288d45c996f
Yigit Kucuk <yigitk@users.noreply.github.com>
2020-10-18 11:25:43 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 apt-get update -qq ; \
 apt-get install -y -qq apt-transport-https && \
 apt-get update -qq && \
-apt-get install -y -qq dotnet-sdk-3.1
-
+apt-get install -y -qq dotnet-sdk-3.1 && \
 dotnet ./src/Task.Main.dll
 
